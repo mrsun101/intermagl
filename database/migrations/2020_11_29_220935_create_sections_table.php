7f9d7862b04e1b2d76f4f55617959c803d514080
Andrey Shpigun <mrsun@chingold.ru>
2020-12-05 23:05:56 +0300

    добавлены миграции и сиды

--- database/migrations/2020_11_29_220935_create_sections_table.php
+++ database/migrations/2020_11_29_220935_create_sections_table.php
@@ -4,20 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSectionsTable extends Migration
-{
+class CreateSectionsTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('sections', function (Blueprint $table) {
-$table->id();
-$table->string('name');
-$table->integer('sort');
-$table->tinyInteger('status'); 
+            $table->id();
+            $table->string('name');
+            $table->integer('sort');
+            $table->tinyInteger('status');
         });
     }
 
@@ -26,8 +25,8 @@ $table->tinyInteger('status');
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('sections');
     }
+
 }
